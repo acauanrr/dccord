cb6c1abc6e714a04b4742ed58c5db18bc4f919be
Acauan Ribeiro <acauan.ribeiro@gmail.com>
2022-10-21 18:15:11 -0400

    Signed-off-by: Acauan Ribeiro <acauan.ribeiro@gmail.com>
    api url cloud front

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     JWT_SECRET:
       "441e4b132dad878cd8865682af4904042a9a3316c1ef62ac7568ec18d4c85e75",
     API_URI_LOCAL: "http://localhost:4001",
-    API_URI_CLOUD: "",
+    API_URI_CLOUD: "https://dccord-acauanrr.vercel.app",
   },
 };
 
